a068990505fd88cb319c2de7cfeef5785268fa09
ProofofLearning <156268750+ProofofLearning@users.noreply.github.com>
2024-02-07 15:52:32 +0800

    Add files via upload

--- 5run_verification.py
+++ 5run_verification.py
@@ -1,16 +1,20 @@
 import subprocess
 import time
 
+# 要重复运行的Python文件
+# python_file = "plot_process_main(new_mec-statistic).py"
+# python_file = "plot_process_main(new_mec-statistic)-sec.py"
 python_file = "data_processing\\k-s_test_verification.py"
 
-delay_seconds = 50
+# 重复运行之间的延迟时间（秒）
+delay_seconds = 20
 
-log_index_list = list(range(1, 2))
-pretrained_names = ['densenet121'] #, 'resnet50'
+log_index_list = list(range(2, 3))
+pretrained_names = ['resnet18'] #, 'resnet50'
 
 #reference 1
-reference_index1 = 1
-reference_name1 = 'densenet121'
+reference_index1 = 2
+reference_name1 = 'resnet18'
 
 for log_index in log_index_list:
     for model_name in pretrained_names:
