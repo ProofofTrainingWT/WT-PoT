b547c19cd25d73d48ba485338921f5c335f131be
ProofofLearning <156268750+ProofofLearning@users.noreply.github.com>
2024-10-19 09:50:27 +0800

    Add files via upload

---
+++ Models/__init__.py
@@ -0,0 +1,10 @@
+from .C_resnet import resnet18, resnet34, resnet50
+from .U_Net_Zoo import *
+from .C_resnet_lowpre import resnet18_lowpre
+from resnet_split import wide_resnet28_10, wide_resnet101_2, wide_resnet50_2, wide_resnet50_3
+from vgg import *
+from squeezenet import *
+from densenet import *
+from alexnet import *
+from vision_transformer import *
+from swin_transformer import *
